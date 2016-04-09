6084e17e799810026178593f713185ca71ea8e38
ajrisylhasi <ajrisylhasi@gmail.com>
2016-04-09 13:53:51 +0000

    Layouts and users

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -5,5 +5,29 @@ class UserControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
-
+  
+  test "should be valid" do 
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name=""
+    assert_not @user.valid?   
+  end
+  test "email should be present" do 
+    @user.email.empty?
+    assert_not @user.valid?
+  end
+  
+  test "name should not be too long" do
+    @user.name="a"*51
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long " do
+    @user.email="a"*244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  
 end
